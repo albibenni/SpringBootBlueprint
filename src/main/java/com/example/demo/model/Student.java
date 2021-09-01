762de6c77c2f2c6ded3c337d78cfa3a3fdcef049
Alberto <alby_b_@hotmail.it>
2021-09-01 17:02:39 +0200

    starting model - student

---
+++ src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,11 @@
+package com.example.demo.model;
+
+import java.time.LocalDate;
+
+public class Student {
+    private Long id;
+    private String name;
+    private String mail;
+    private LocalDate dob;
+    private Integer age;
+}
