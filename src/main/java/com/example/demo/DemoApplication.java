5d630ad70dd7f091f7188a0b29c15b7d46ac9f0b
benni <alby_b_@hotmail.it>
2021-09-04 10:33:49 +0200

    model tested with localhost 8081 changed view 'helloTry' method returning a list of students

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -15,7 +19,15 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public String helloTry(){
-		return "hello world";
+	public List<Student> helloTry(){
+		return List.of(
+				new Student(
+						1L,
+						"Luca",
+						"luca@gmail.com",
+						LocalDate.of(2000, 07, 15),
+						21
+				)
+		);
 	}
 }
--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -8,4 +8,74 @@ public class Student {
     private String mail;
     private LocalDate dob;
     private Integer age;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", mail='" + mail + '\'' +
+                ", dob=" + dob +
+                ", age=" + age +
+                '}';
+    }
+
+    public Student() {
+    }
+
+    public Student(String name, String mail, LocalDate dob, Integer age) {
+        this.name = name;
+        this.mail = mail;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Student(Long id, String name, String mail, LocalDate dob, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.mail = mail;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
 }
