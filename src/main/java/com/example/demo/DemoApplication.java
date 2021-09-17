c90a8a19738a81bd75858a51632d7305870e468c
benni <alby_b_@hotmail.it>
2021-09-17 21:33:13 +0200

    service added with injection

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,10 @@
 package com.example.demo;
 
-import com.example.demo.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
@@ -18,16 +12,5 @@ public class DemoApplication {
 				.run(DemoApplication.class, args);
 	}
 
-	@GetMapping
-	public List<Student> helloTry(){
-		return List.of(
-				new Student(
-						1L,
-						"Luca",
-						"luca@gmail.com",
-						LocalDate.of(2000, 07, 15),
-						21
-				)
-		);
-	}
+
 }
